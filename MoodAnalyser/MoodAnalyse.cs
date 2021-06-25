d3a6b4f1cfa21566c50eeb6d7d8a7da7b2888348
Harsh <harsh.21483@gmail.com>
2021-06-25 08:27:02 -0700

    [Harsh]Add:-New class of Moodanalyse is added and a MSTest file is added

---
+++ MoodAnalyser/MoodAnalyse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyser
+{
+    public class MoodAnalyse
+    {
+        private string message;
+        public MoodAnalyse(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood()
+        {
+            if (this.message.Contains("SAD"))
+            {
+                return "SAD";
+            }
+            else
+                return "HAPPY";
+        }
+    }
+}
---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoodAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoodAnalyse mood = new MoodAnalyse("SAD");
+            mood.AnalyseMood();
+        }
+    }
+}
