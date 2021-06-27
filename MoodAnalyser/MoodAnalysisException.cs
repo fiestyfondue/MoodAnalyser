1e97e202827d513a729d1f57f72536fcbabc74ee
Harsh <harsh.21483@gmail.com>
2021-06-26 20:26:32 -0700

    [Harsh]Add:- New class for MoodAnalysisException is added

---
+++ MoodAnalyser/MoodAnalysisException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyser
+{
+    class MoodAnalysisException : Exception
+    {
+        public enum Errors{Empty};
+
+        // custom exception for mood analysis. display an empty or null message
+        public MoodAnalysisException()
+        {
+            Console.WriteLine(Errors.Empty);
+        }
+    }
+}
